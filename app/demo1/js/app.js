956fe3c46c7c8611d2275bd34b8ca9d0e71c73f3
tiantianquan <fenqiufen@gmail.com>
2014-12-18 00:13:00 +0800

    count down demo

--- app/demo1/js/app.js
+++ app/demo1/js/app.js
@@ -1,15 +1,16 @@
 angular.module('demo-app', [])
 
 .controller('Ctrl', function($scope) {
-  $scope.targetDate = new Date('2014-12-17 22:29:0')
-  $scope.$broadcast('countDown.targetDateChanged')
+  $scope.init = function() {
+    // $scope.targetDate = new Date('2014-12-18 23:29:0')
 
-  $scope.targetDateChanged = function() {
-    $scope.$broadcast('countDown.targetDateChanged')
+    $scope.targetDateChanged = function() {
+      $scope.$broadcast('countDown.targetDateChanged')
+    }
   }
 })
 
-.directive('countDown', function($interval) {
+.directive('countDown', function($interval, $timeout, $rootScope) {
   return {
     restrict: 'E',
     scope: {
@@ -17,32 +18,42 @@ angular.module('demo-app', [])
     },
     templateUrl: 'js/count-down.template.html',
     link: function(scope, element, attr) {
-      var time,inter
+      var time, inter
 
       scope.$on('countDown.targetDateChanged', function() {
+        $timeout(function() {
+          go()
+        }, 0)
+      })
+
+      go()
+
+      function go() {
+        console.log(scope)
         $interval.cancel(inter)
+
         time = Math.round((scope.targetDate - Date.now()) / 1000)
-        if (time <= 0) {
+        if (time > 0) {
+          scope.countTime = convertTime(time)
+
+        } else {
           scope.countTime = {
             days: 0,
             hours: 0,
             minutes: 0,
             seconds: 0
           }
-        } else {
-          scope.countTime = convertTime(time)
         }
 
         inter = $interval(function() {
-          if (time <= 0) {
-            $interval.cancel(inter)
-          } else {
+          if (time > 0) {
             time--
             scope.countTime = convertTime(time)
+          } else {
+            $interval.cancel(inter)
           }
         }, 1000)
-      })
-
+      }
 
     },
   }
